0e28fe0fab1389f7c0f9b5f5f9808c4980517aa2
emontes <emontes@turista.com.mx>
2019-10-27 20:18:09 -0500

    Updates

---
+++ precache-manifest.999e23e91f1d2c1a13b0c5bc88eade05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89a76345115b5c715a09e350aae846f2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d3d9ae6eca4c01a813be",
+    "url": "/monsters-rolodex/static/css/main.a9a669b2.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "d3d9ae6eca4c01a813be",
+    "url": "/monsters-rolodex/static/js/main.dcf55430.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
